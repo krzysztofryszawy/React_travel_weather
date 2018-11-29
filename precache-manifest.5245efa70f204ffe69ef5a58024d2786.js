7e0f2200f580a00452864a1ed543f375a1609b34
Krzysztof Ryszawy <krzysztof.ryszawy@gmail.com>
2018-11-29 16:39:50 +0100

    Updates

---
+++ precache-manifest.5245efa70f204ffe69ef5a58024d2786.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "e1832399d178c2c757acd91d97412005",
+    "url": "/React_travel_weather/static/media/vertical-waves.e1832399.png"
+  },
+  {
+    "revision": "98219f719c309818fbc1357d69fa87c1",
+    "url": "/React_travel_weather/static/media/tutorial_2.98219f71.png"
+  },
+  {
+    "revision": "57ce39848f6129130a51002044a1abdd",
+    "url": "/React_travel_weather/static/media/tutorial_1.57ce3984.png"
+  },
+  {
+    "revision": "5e5587d4038e73e2dce3d2a728b0e3ad",
+    "url": "/React_travel_weather/static/media/travel.5e5587d4.jpg"
+  },
+  {
+    "revision": "0c2cf9a3192a32c68275dc896430dadd",
+    "url": "/React_travel_weather/static/media/simon-rae-732820-unsplash_1920.0c2cf9a3.jpg"
+  },
+  {
+    "revision": "f1c34bc72977dad68fd6",
+    "url": "/React_travel_weather/static/js/runtime~main.f1c34bc7.js"
+  },
+  {
+    "revision": "f7ae70c05d3e1f073613",
+    "url": "/React_travel_weather/static/js/main.f7ae70c0.chunk.js"
+  },
+  {
+    "revision": "8cc93cbb6542f0cd6c12",
+    "url": "/React_travel_weather/static/js/1.8cc93cbb.chunk.js"
+  },
+  {
+    "revision": "f7ae70c05d3e1f073613",
+    "url": "/React_travel_weather/static/css/main.e0bfc6b1.chunk.css"
+  },
+  {
+    "revision": "9dd9913147797931dde0f16829b73ef7",
+    "url": "/React_travel_weather/index.html"
+  }
+];
